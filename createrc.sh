41074b664d53f00cf2d009da67ba69dcbffed358
Brian Mackey <mackey.brian@gmail.com>
2016-09-03 13:23:29 -0500

    updated notes

--- createrc.sh
+++ createrc.sh
@@ -1,9 +1,11 @@
 # ABOUT
 # createrc.sh:  This script assumes you create a new rc branch with every release.
-#  Step 1 will delete rename the current rc branch to rcDATE_STAMP
-#  Step 1 will then delete current rc from local and remote
-#  Step 1 will skip the delete process in the case where rename rc fails
-#  Step 2 creates a new rc branch based off the latest version of master
+# Long living branches are evil.  This script creates a fresh rc branch based off of master.
+# The intention is to run this script with each new release
+# Step 1 will delete rename the current rc branch to rcDATE_STAMP
+# Step 1 will then delete current rc from local and remote
+# Step 1 will skip the delete process in the case where rename rc fails
+# Step 2 creates a new rc branch based off the latest version of master
 # arguments:  1st argument if exists will put the script into test mode.  E.G. createrc.sh test
 # Test mode:  assumes rc branch does not exist.  It will simply create a new rc branch for you.
 # Test mode:  I did not add a delete function to test mode for safety purposes in a production environment
