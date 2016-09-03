856b32ab378a0cd8fddf1759dc320f96ac24056c
Brian Mackey <mackey.brian@gmail.com>
2016-09-03 17:16:24 -0500

    update notes

--- createrc.sh
+++ createrc.sh
@@ -3,11 +3,12 @@
 # Long living branches are evil.  This script creates a fresh rc branch based off of master.
 # The intention is to run this script with each new release
 # The script runs in 2 steps.
-# Step 1 will delete rename the current rc branch to rcDATE_STAMP
-# Step 1 will then delete current rc from local and remote
-# Step 1 will skip the delete process in the case where rename rc fails
-# Step 2 creates a new rc branch based off the latest version of master
-# arguments:  1st argument if exists will put the script into test mode.  E.G. createrc.sh test
+# Step 1:
+#        - rename the current rc branch to rcDATE_STAMP
+#        - if rename successful, delete current rc from local and remote
+# Step 2:
+#         - create a new rc branch based off the latest version of master
+# argument 1: put the script into test mode.  E.G. createrc.sh test
 # Test mode:  assumes rc branch does not exist.  It will simply create a new rc branch for you.
 # Test mode:  I did not add a delete function to test mode for safety purposes in a production environment
 
@@ -25,9 +26,9 @@ if [ $1 ]
 fi
 
 #######1 rename the old rc branch#####
-getCo="git checkout rc"
+gitCo="git checkout rc"
 getLatest="git pull"
-${getCo}
+${gitCo}
 ${getLatest}
 
 #get todays date for use with renaming rc
@@ -38,14 +39,14 @@ newrcName="rc$Now_hourly"
 #echo "$newrcName"
 
 #rename rc to the new name
-getRename="git branch -m $newrcName"
+gitRename="git branch -m $newrcName"
 #Delete the old remote rc branch
 deleteOldBranchRemote="git push origin --delete rc"
 deleteOldBranchLocal="git branch -D rc"
 #Reset the upstream branch for the new-name local branch.
 resetUpstream="git push -u origin $newrcName"
 
-${getRename}
+${gitRename}
 # $? contains the exit code of the preceding echo
 BRANCH_EXIT_CODE=$?
 if [ $BRANCH_EXIT_CODE -eq 0 ];
@@ -61,7 +62,7 @@ ${resetUpstream}
 #######2 create the new branch#######
 
 getCoMaster="git checkout master"
-${getCoMaster}
+${gitCoMaster}
 ${getLatest}
 
 createNewRc="git checkout -b rc"
